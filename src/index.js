8c2b9e9a5f62aae2721aa91b812acf3d1999f596
hachejorge <872838@unizar.es>
2025-06-24 13:57:51 +0200

    Preparando despliegue

--- src/index.js
+++ src/index.js
@@ -26,6 +26,8 @@ app.use(authenticateToken, createRoutes);
 import deleteRoutes from "./routes/delete.routes.js";
 app.use(authenticateToken, deleteRoutes);
 
-app.listen(3000, () => {
-    console.log("Server is running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
